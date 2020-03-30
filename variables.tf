b012808d6eeb6438af2dae9289595268dc58cb0f
Rohit Amarnath <rohit.amarnath@full360.com>
2020-03-30 10:12:23 -0400

    terraform v0.12 update

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "parameter_read" {
-  type        = "list"
+  type        = list(string)
   description = "List of parameters to read from SSM. These must already exist otherwise an error is returned. Can be used with `parameter_write` as long as the parameters are different."
   default     = []
 }
 
 variable "parameter_write" {
-  type = "list"
+  type = list
 
   description = <<DESC
   List of maps with the Parameter values in this format.
@@ -16,33 +16,34 @@ variable "parameter_write" {
     type = "SecureString" // Required - Valid types are String, StringList and SecureString
     value = "password1" // Required
     description = "Production database master password" // Optional
-    overwrite = false // Optional - Force Overwrite of value if true. 
+    overwrite = false // Optional - Force Overwrite of value if true.
   }]
 DESC
+
 
   default = []
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map(string)
   default     = {}
   description = "Map containing tags that will be added to the parameters"
 }
 
 variable "kms_arn" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "The ARN of a KMS key used to encrypt and decrypt SecretString values"
 }
 
 variable "enabled" {
-  type        = "string"
+  type        = string
   default     = "true"
   description = "Set to `false` to prevent the module from creating and accessing any resources"
 }
 
 variable "split_delimiter" {
-  type        = "string"
+  type        = string
   default     = "~^~"
   description = "A delimiter for splitting and joining lists together for normalising the output"
 }
